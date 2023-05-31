b278bf4b3d6fdddb1be419d006efdbdde18ecd9c
GLaskaridis <icsd14107@icsd.aegean.gr>
2023-05-31 15:02:03 +0300

    Tested

--- src/main/java/DataSourceConfig.java
+++ src/main/java/DataSourceConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataSourceConfig {
     public DataSource getDataSource(){
+        System.out.println(System.getenv("SPRING_DB_URL"));
         return DataSourceBuilder.create()
         .driverClassName("com.mysql.cj.jdbc.Driver")
         .url(System.getenv("SPRING_DB_URL"))
