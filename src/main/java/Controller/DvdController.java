db53d4f2bbdfab8a2ccbae89547c702cf69e2b4c
GLaskaridis <icsd14107@icsd.aegean.gr>
2023-08-09 12:37:00 +0300

    final update

--- src/main/java/Controller/DvdController.java
+++ src/main/java/Controller/DvdController.java
@@ -1,7 +1,10 @@
 package Controller;
 
-
+import Controller.Dvd;
+import Controller.DvdRepository;
+import Controller.GenreClass;
 import Model.Genre;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,87 +23,134 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class DvdController {
+
     @Autowired
     DvdRepository dvdrepo;
-    DvdController(){}
-    
-    @PostMapping("/dvd/add")
-    //JSON
-    public String addDvd(@RequestBody Dvd dvd){
-        String input = dvd.getTitle();
-        List temp = dvdrepo.findByTitle(input);
-        
-        if(temp.isEmpty()){
-            dvdrepo.save(dvd);
-        return "Created";
-        }
-        else
-            return "Already exists";
+
+    DvdController() {
     }
 
- @GetMapping("/dvds")
-public ResponseEntity<?> searchDvd(@RequestParam(required = false) String search) {
-    if (search == null || search.equals("all")) {
-        List<Dvd> dvdList = dvdrepo.findAll();
-        return new ResponseEntity<>(dvdList, HttpStatus.OK);
-    } else if (search.matches("\\d+")) {
+    @PostMapping("/dvd/add")
+    //JSON
+    public ResponseEntity<?> addDvd(@RequestBody Dvd dvd) {
+        String input = dvd.getTitle();
+        List temp = dvdrepo.findByTitle(input);
+
+        //elegxos gia titlo
+        if (dvd.getTitle().isEmpty()) {
+            return new ResponseEntity<>("DVD should have a title", HttpStatus.BAD_REQUEST);
+        }
+
+        //elegxos temaxiwn dvd
+        if (dvd.getAvailable() <= 0) {
+            return new ResponseEntity<>("Wrong DVD items!", HttpStatus.BAD_REQUEST);
+        }
+
+        //elegxos swstou genre gia dvd
+        try {
+            Genre genre = Genre.valueOf(dvd.getGenre().toString());
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("DVD genre not exists!", HttpStatus.BAD_REQUEST);
+        }
+
+        if (temp.isEmpty()) {
+            dvdrepo.save(dvd);
+            return new ResponseEntity<>("DVD Created!", HttpStatus.OK);
+        } else //ean yparxei idi o titlos
+        {
+            return new ResponseEntity<>("Title of dvd already exists", HttpStatus.CONFLICT);
+        }
+    }
+
+    public ResponseEntity<?> searchTitle(String search) {
+        List<Dvd> dvdList = dvdrepo.findByTitle(search);
+        if (!dvdList.isEmpty()) {
+            return new ResponseEntity<>(dvdList, HttpStatus.OK);
+        }
+        else return new ResponseEntity<>("Title or ID Not Found", HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<?> searchID(String search) {
         long id = Long.parseLong(search);
         Optional<Dvd> dvd = dvdrepo.findById(id);
         if (dvd.isPresent()) {
             return new ResponseEntity<>(dvd.get(), HttpStatus.OK);
         }
-    } else {
-        List<Dvd> dvdList = dvdrepo.findByTitle(search);
-        if (!dvdList.isEmpty()) {
+        else return new ResponseEntity<>("Title or Id Not Found", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/dvds")
+    public ResponseEntity<?> searchDvd(@RequestParam(required = false) String search) {
+        if (search == null || search.equals("all")) {
+            List<Dvd> dvdList = dvdrepo.findAll();
             return new ResponseEntity<>(dvdList, HttpStatus.OK);
+        } else if (search.matches("\\d+")) {
+            return searchID(search);
+
+        } else if (search.matches("[a-zA-Z0-9\\s]+")) {
+            return searchTitle(search);
+        }
+        else {
+           return new ResponseEntity<>("Bad Request ", HttpStatus.BAD_REQUEST);
         }
     }
-    List<Dvd> dvdList = dvdrepo.findAll();
-    return new ResponseEntity<>(dvdList, HttpStatus.OK);
-}
 
     @DeleteMapping("/dvd/delete/{key}")
-public ResponseEntity<?> deleteDvd(@PathVariable("key") long key) {
-    Optional<Dvd> dvd = dvdrepo.findById(key);
-    if (dvd.isPresent()) {
-        dvdrepo.deleteById(key);
-        return new ResponseEntity<>("Dvd with id " + key + " has been deleted", HttpStatus.OK);
-    } else {
-        return new ResponseEntity<>("Dvd with id " + key + " does not exist", HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> deleteDvd(@PathVariable("key") long key) {
+        Optional<Dvd> dvd = dvdrepo.findById(key);
+        if (dvd.isPresent()) {
+            dvdrepo.deleteById(key);
+            return new ResponseEntity<>("Dvd with id " + key + " has been deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Dvd with id " + key + " does not exist", HttpStatus.NOT_FOUND);
+        }
     }
-}
 
+    @PutMapping("/dvd/updateGenre/{key}")
+    public ResponseEntity<?> updateDvdGenre(@PathVariable("key") Long id, @RequestBody GenreClass genre) {
 
-@PutMapping("/dvd/updateGenre/{key}")
-//JSON
-public ResponseEntity<Dvd> updateDvdGenre(@PathVariable("key") Long id, @RequestBody GenreClass genre) {
-    
-    Optional<Dvd> dvdOptional = dvdrepo.findById(id);
-    if (!dvdOptional.isPresent()) {
-        return ResponseEntity.notFound().build();
+        if (id == null)
+            return new ResponseEntity<>("Dvd with id " + id + " does not exist", HttpStatus.NOT_FOUND);
+        
+        Optional<Dvd> dvdOptional = dvdrepo.findById(id);
+        if (!dvdOptional.isPresent()) {
+            return new ResponseEntity<>("Dvd with id " + id + " does not exist", HttpStatus.NOT_FOUND);
+        }
+        Dvd dvd = dvdOptional.get();
+        Genre newgenre;
+        //elegxos swstou genre gia dvd
+        try {
+            newgenre = Genre.valueOf(dvd.getGenre().toString());
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("DVD genre not exists!", HttpStatus.BAD_REQUEST);
+        }
+        
+        dvd.setGenre(newgenre);
+        dvdrepo.save(dvd);
+        return new ResponseEntity<>("Dvd with id " + id + " has been deleted", HttpStatus.OK);
     }
-    Dvd dvd = dvdOptional.get();
-    dvd.setGenre(Genre.valueOf(genre.getGenre()));
-    dvdrepo.save(dvd);
-    return ResponseEntity.ok(dvd);
-}
 
-
-   @PutMapping("/dvd/update/available/{id}")
-public ResponseEntity<?> updateDvdAvailable(@PathVariable("id") long id, @RequestParam("available") int available) {
-    Optional<Dvd> dvd = dvdrepo.findById(id);
-    if (dvd.isPresent()) {
-        dvd.get().setAvailable(available);
-        dvdrepo.save(dvd.get());
-        return new ResponseEntity<>("Dvd available quantity updated successfully", HttpStatus.OK);
-    } else {
-        return new ResponseEntity<>("Dvd with id " + id + " does not exist", HttpStatus.NOT_FOUND);
+    @PutMapping("/dvd/update/available/{id}")
+    public ResponseEntity<?> updateDvdAvailable(@PathVariable("id") Long id, @RequestParam("available") int available) {
+        if (id == null)
+            return new ResponseEntity<>("Wrong id", HttpStatus.NOT_FOUND);
+        
+         //elegxos temaxiwn dvd
+        if (available <= 0) {
+            return new ResponseEntity<>("Wrong DVD items!", HttpStatus.BAD_REQUEST);
+        }
+        
+        Optional<Dvd> dvd = dvdrepo.findById(id);
+        
+        if (dvd.isPresent()) {
+            dvd.get().setAvailable(available);
+            dvdrepo.save(dvd.get());
+            return new ResponseEntity<>("Dvd available quantity updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Dvd with id " + id + " does not exist", HttpStatus.NOT_FOUND);
+        }
     }
-}
 
-    
-    
 }
--- src/main/java/DataSourceConfig.java
+++ src/main/java/DataSourceConfig.java
@@ -8,9 +8,15 @@ import org.springframework.context.annotation.Configuration;
 public class DataSourceConfig {
     public DataSource getDataSource(){
         System.out.println(System.getenv("SPRING_DB_URL"));
-        return DataSourceBuilder.create()
+        /*return DataSourceBuilder.create()
         .driverClassName("com.mysql.cj.jdbc.Driver")
         .url("jdbc:mysql://db:3306/dvddb?autoReconnect=true&useSSL=false")
+        .username("Giorgos")
+        .password("1234")
+        .build();*/
+        return DataSourceBuilder.create()
+        .driverClassName("com.mysql.cj.jdbc.Driver")
+        .url("jdbc:mysql://localhost:3306/dvddb?autoReconnect=true&useSSL=false")
         .username("Giorgos")
         .password("1234")
         .build();
